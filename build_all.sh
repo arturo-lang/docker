aaa8388381ed99081e16a54ec4dee3f6ba7664e5
drkameleon <yaniszaf@gmail.com>
2026-01-16 13:32:05 +0100

    removed irrelevant files

--- build_all.sh
+++
@@ -1,3 +0,0 @@
-# use --no-cache for clean re-build
-docker build -t arturo .
-docker run -ti arturo
--- build_all_mini.sh
+++
@@ -1,3 +0,0 @@
-# use --no-cache for clean re-build
-docker build -f Dockerfile-mini -t arturo-mini .
-docker run -ti arturo-mini
